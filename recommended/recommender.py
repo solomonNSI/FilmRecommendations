6e210ae3757586f34b11998ed24ce589c703c390
tomas <tom.hruby1@gmail.com>
2022-05-13 15:44:19 +0200

    recommender engine prototype

--- recommended/recommender.py
+++ recommended/recommender.py
@@ -1,12 +1,32 @@
-class RecommenderEngine:
+class RecommenderEngine():
+    
+    def __init__(self, similarity_path):
+        self.similarity = np.load(similarity_path)
 
-    def __init__(self):
-        '''Add the implementation here, load from DB'''
-        print('blabla')
-        #self.recommender = something(model)
+    def recommend(self, in_mov, n=5, ratings=None):
+        #similarity of all m in M to I := S
+        #in_mov = #[23076, 911] #= interstellar, space odyssey
+        
+        S = np.zeros(self.similarity.shape[0])
 
-    def recommend(self, movie_id, n=5):
-        '''lets use func. like this to get the recommendation
-            return Title (or Id) of the movie'''
-
-        return 'The Lord of The Rings: The Fellowship of the Ring'
+        BETA = 0.5
+        def movie_rating(idx):
+                if ratings != None:
+                    rat = df.iloc[idx]['vote_average']
+                    #count = df.iloc[idx]['vote_count'] TODO weighted vote
+                    if rat > 0:
+                        return rat
+                return 0
+                    
+        for m in range(len(self.similarity)):
+            s = 0 #total sim to movies
+            for i in in_mov:  #stupid, just half is enough
+                if m != i:
+                    s += self.similarity[m,i] + movie_rating(m) * BETA
+            S[m] = s
+            
+        # #now argsort again and find several best ones
+        print("max score:", np.max(S), " for id:", np.argmax(S))
+        sort = np.argsort(S)
+        
+        return np.flip(sort[-n:])
